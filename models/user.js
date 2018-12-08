4c345f39e256cb3220da3456050c402b766f6e8b
KhallilB <khallil.bailey@gmail.com>
2018-12-08 13:13:15 -0800

    user model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  },
+  password: {
+    type: String,
+    select: false
+  },
+  usernmae: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("User", UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -3,4 +3,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 //AUTH MODEL
 
+//GET SIGNUP FORM
+router.get("/sign-up", (req, res) => {
+  res.render("sign-up");
+});
+
 module.exports = router;
